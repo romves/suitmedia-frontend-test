a98b22af3f97cbe2fb36d6f5bb4bfdaba57faafe
romves <nofalrmz@gmail.com>
2025-01-19 00:50:59 +0700

    chore: add image host and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'suitmedia.static-assets.id'
+      },
+      {
+        hostname: 'images.unsplash.com',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
