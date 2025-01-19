ae7b39e8164d103c8aac8bdc93ecfb6aa71bb7ca
romves <nofalrmz@gmail.com>
2025-01-19 08:42:05 +0700

    feat(refactor): update searchParams type to Promise and improve parameter resolution

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,16 @@
 import HomeContainer from '@/features/ideas/container/HomeContainer';
 
 interface HomeProps {
-  searchParams: { [key: string]: string | undefined };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 export default async function Home({ searchParams }: HomeProps) {
-  const { page, perPage, sortBy } = await searchParams;
+  const resolvedParams = await searchParams;
 
   const params = {
-    page: page ? page : '1',
-    perPage: perPage ? perPage : '10',
-    sortBy: sortBy || '-published_at',
+    page: (resolvedParams.page as string) || '1',
+    perPage: (resolvedParams.perPage as string) || '10',
+    sortBy: (resolvedParams.sortBy as string) || '-published_at',
   };
 
   return <HomeContainer searchParams={params} />;
