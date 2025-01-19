6020a1ec924d2a7c8e07460887836b3932cf638f
romves <nofalrmz@gmail.com>
2025-01-19 08:28:44 +0700

    feat(release): remove React Query Devtools

--- src/shared/providers/query-provider.tsx
+++ src/shared/providers/query-provider.tsx
@@ -1,18 +1,16 @@
-'use client'
+'use client';
 
-import React from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
 
-export default function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = React.useState(() => new QueryClient({
-
-  }))
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = React.useState(() => new QueryClient({}));
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  )
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
